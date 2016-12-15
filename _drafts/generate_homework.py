4b12736ba06c892453d505268a9eb42ec4c43018
aylvisaker <ajylvisaker@gmail.com>
2016-12-15 11:49:57 -0600

    reformat homework page

---
+++ _drafts/generate_homework.py
@@ -0,0 +1,16 @@
+import csv
+
+hw_markup = open('homework.md', 'w')
+assignments = list(csv.reader(open('homework.csv', 'rU')))
+
+hw_markup.write('---\nlayout: post\ntitle: "Course Schedule"\n')
+hw_markup.write('categories: [syllabus]\ntags: [syllabus]\n')
+hw_markup.write('description: MC-MATH-141\n---\n\n')
+
+for hw in assignments[1:]:
+	section = '* Section ' + str(hw[0]) + ' homework:\n'
+	practice = '    * practice problems: '     + str(hw[1]) + '\n'
+	easy     = '    * easy problems: '         + str(hw[2]) + '\n'
+	hard     = '    * hard problems: '         + str(hw[3]) + '\n'
+	xcr      = '    * extra credit problems: ' + str(hw[4]) + '\n'
+	hw_markup.write(section + practice + easy + hard + xcr)
